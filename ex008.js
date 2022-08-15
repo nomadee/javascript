6e6635afc4bf0aed9049263f3cc3bce266744588
Gabriel Vinicius <gabrielvinicius-hatake@hotmail.com>
2022-08-15 20:05:42 -0300

    ,

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condicao simples
+    console.log(`Voce utrapassou a velocidade permitida`)
+
+} 
+console.log(`dirija sempre usando cinto de seguranca!`)
---
+++ exercicios/ex010.js
@@ -0,0 +1,10 @@
+ var pais ='Brasil'
+
+ console.log(`vivendo em ${pais}`)
+
+ if (pais != 'Brasil') {
+    console.log('Voce é estrangeiro')
+ }
+ else {
+    console.log('Voce é brasileiro')
+ }
