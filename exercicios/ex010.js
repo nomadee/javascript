fca8be126904c71eae20bef462d7a8c644597f94
Gabriel Vinicius <gabrielvinicius-hatake@hotmail.com>
2022-08-17 19:17:15 -0300

    ,

--- exercicios/ex010.js
+++ exercicios/ex010.js
@@ -2,9 +2,9 @@
 
  console.log(`vivendo em ${pais}`)
 
- if (pais != 'Brasil') {
-    console.log('Voce é estrangeiro')
+ if (pais == 'Brasil') {
+    console.log('Voce é brasileiro')
  }
  else {
-    console.log('Voce é brasileiro')
+    console.log('Voce é estrangeiro')
  }
